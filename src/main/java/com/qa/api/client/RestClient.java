290bcfd5fc9245437bee6b0eb2bc0c228e8e22f5
Naveen AutomationLabs <naveenanimation20@gmail.com>
2024-11-07 20:21:28 +0530

    updated rest client

--- src/main/java/com/qa/api/client/RestClient.java
+++ src/main/java/com/qa/api/client/RestClient.java
@@ -37,6 +37,7 @@ public class RestClient {
 	private ResponseSpecification responseSpec422 = expect().statusCode(422);
 	private ResponseSpecification responseSpec500 = expect().statusCode(500);
 	private ResponseSpecification responseSpec501 = expect().statusCode(501);
+	private ResponseSpecification responseSpec502 = expect().statusCode(502);
 
 	
 	
